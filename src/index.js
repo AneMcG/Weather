10d14e3eb208cf9b7eab7fa295f59dc563aaf27b
AneMcG <71961266+AneMcG@users.noreply.github.com>
2020-11-26 21:02:30 +1300

    added location search

--- src/index.js
+++ src/index.js
@@ -1,52 +1,40 @@
-//Time stamp
+//Time and Date
 let now = new Date();
+
 let hours = now.getHours();
+
 let minutes = now.getMinutes();
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 let day = days[now.getDay()];
+
 let date = now.getDate();
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let month = months[now.getMonth()];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-let timeStamp = document.querySelector("#time-stamp");
-timeStamp.innerHTML = `${hours}:${minutes} - ${day}, ${date} ${month}`;
+let month = months[now.getMonth()]
 
+let timeStamp = document.querySelector("#time-stamp")
+timeStamp.innerHTML = (` ${hours}:${minutes} - ${day}, ${date} ${month}`)
 
-//Weather API
+//weather API
 function displayWeather(response) {
     document.querySelector("#my-location").innerHTML = response.data.name;
-    let temperature = document.querySelector("#location-temperature");
-    temperature.innerHTML = Math.round(response.data.main.temp);
-    let tempMin = document.querySelector("#temp-min")
-    tempMin.innerHTML = Math.round(response.data.main.temp_min);
-    let tempMax = document.querySelector("#temp-max");
-    tempMax.innerHTML = Math.round(response.data.main.temp_max);
-    let humidity = document.querySelector("#humidity");
-    humidity.innerHTML = response.data.main.humidity;
-    let wind = document.querySelector("#wind-speed");
-    wind.innerHTML = Math.round(response.data.wind.speed);
-    let wind = document.querySelector("#wind-direction");
-    wind.innerHTML = Math.round(response.data.wind.direction);
-    document.querySelector("#description").innerHTML =
-        response.data.weather[0].main;
+    document.querySelector("#sunrise").innerHTML = response.data.sys.sunrise;
+    document.querySelector("#sunset").innerHTML = response.data.sys.sunset;
+    document.querySelector("#location-temperature").innerHTML = Math.round(response.data.main.temp);
+    document.querySelector("#temp-min").innerHTML = Math.round(response.data.main.temp_min);
+    document.querySelector("#temp-max").innerHTML = Math.round(response.data.main.temp_max);
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind-speed").innerHTML = response.data.wind.speed;
+    document.querySelector("#wind-direction").innerHTML = response.data.wind.deg;
+    document.querySelector(newFunction())
+    response.data.weather[0].description;
+
+    function newFunction() {
+        return "#description";
+    }
 }
 
-function search(city) {
-    let key = "ef3fad40f719ac8a63ea791f6953bb62";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-    axios.get(url).then(displayWeather);
-}
-
-function searchCity(event) {
-    event.preventDefault();
-    let city = document.querySelector("input").value;
-    search(city);
-}
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchCity);
-
-//current Location
+//current location default 
 function currentLocation(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
@@ -59,3 +47,25 @@ function currentLocation(position) {
 function getCurrentLocation() {
     navigator.geolocation.getCurrentPosition(currentLocation);
 }
+
+getCurrentLocation()
+
+
+//search location
+function search(city) {
+    let key = "ef3fad40f719ac8a63ea791f6953bb62";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+
+    axios.get(url).then(displayWeather);
+}
+
+function searchCity(event) {
+    event.preventDefault();
+    let city = document.querySelector("#search-field").value;
+    search(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
+
+//weather forecast 
